64f666e800e46cc60394c19d843898912c7973ac
JOÃO PEDRO FERREIRA PONCHIROLI <joao_ponchiroli@estudante.sesisenai.org.br>
2024-03-13 21:30:50 -0300

    Adding more Exercices

---
+++ src/exercicios/Exe_32.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exe_32 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		double pesoPessoaKG, pesoPessoaG;
+		
+		System.out.println("Informe o peso da pessoa");
+		pesoPessoaKG = s.nextDouble();
+		
+		pesoPessoaG = pesoPessoaKG * 1000;
+		
+		System.out.println("Peso Pessoa em Gramas: " + pesoPessoaG);
+	}
+}
+/*
+	32. Faça um algoritmo que receba o peso de uma pessoa em quilos, calcule e mostre esse peso
+	em gramas.
+*/
---
+++ src/exercicios/Exe_33.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class Exe_33 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		double baseMaior, baseMenor, altura, areaTrapezio;
+		
+		System.out.println("Informe a base maior do trapézio");
+		baseMaior = s.nextDouble();
+		
+		System.out.println("Informe a base menor do trapézio");
+		baseMenor = s.nextDouble();
+		
+		System.out.println("Informe a altura do trapézio");
+		altura = s.nextDouble();
+		
+		areaTrapezio = ((baseMaior + baseMenor) * altura) / 2;
+		
+		System.out.println("Área do Trapézio: " + areaTrapezio);
+	}
+}
+/*
+	33. Faça um algoritmo que calcule e mostre a área de um trapézio. Sabe-se que: A = (base maior +
+	base menor)* altura)/2 ;
+*/
