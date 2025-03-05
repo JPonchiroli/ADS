be980af628644e589d8d58e0187a1ec70740f490
JPonchiroli <joaopedroponqueroli@gmail.com>
2025-03-05 18:41:43 -0300

    refatorando aula01

---
+++ src/3° Semestre/EstruturaDados/src/aula01/Recursivo.java
@@ -0,0 +1,46 @@
+package aula01;
+
+import java.util.Scanner;
+
+public class Recursivo {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Exe 01");
+        System.out.print("Informe um numero: ");
+        int numeroEscolhidoSomarAte = s.nextInt();
+
+
+        System.out.print("Soma Até: " + somarAte(numeroEscolhidoSomarAte));
+
+        System.out.println();
+        System.out.println("------------------------------------------------------------");
+
+        System.out.println("Exe 02");
+        System.out.print("Informe um numero: ");
+        int numeroEscolhidoFatorial = s.nextInt();
+
+
+        System.out.print("Fatorial de " + numeroEscolhidoFatorial + ": " + fatorial(numeroEscolhidoFatorial));
+
+
+    }
+
+    public static int somarAte(int numero){
+        // Caso Base
+        if (numero == 0)
+            return 0;
+
+        // Caso de rcursao
+        return numero + somarAte(numero - 1);
+    }
+
+    public static int fatorial(int numero){
+        // Caso Base
+        if (numero == 0)
+            return 1;
+
+        // Caso de rcursao
+        return numero * fatorial(numero - 1);
+    }
+}
