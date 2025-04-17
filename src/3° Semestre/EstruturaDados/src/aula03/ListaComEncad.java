482ec2f21f92f808e4ec31c7e79e9b516318c9b6
JPonchiroli <joaopedroponqueroli@gmail.com>
2025-04-16 21:32:47 -0300

    adicionando pt2 aula 16/04

--- src/3° Semestre/EstruturaDados/src/aula03/ListaComEncad.java
+++ src/3° Semestre/EstruturaDados/src/aula03/ListaComEncad.java
@@ -6,10 +6,31 @@ public class ListaComEncad {
     private NoLista last;
     private int counter;
 
-    public ListaComEncad(){}
+    public ListaComEncad(){
+        first = null;
+        last = null;
+        counter = 0;
+    }
 
-    public void add(Integer element) {}
-    public void add(int index, Integer element) {}
+    public void add(Integer element) {
+        NoLista temp = new NoLista(element, null);
+        if (counter == 0) {
+            this.first = temp;
+        } else {
+            this.last.setNext(temp);
+        }
+        this.last = temp;
+        counter++;
+    }
+
+    public void add(int index, Integer element) throws ArrayIndexOutOfBoundsException{
+        if (index < 0 || index >= counter) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+
+
+
+    }
     public Integer remove(int index) { return 0;}
     public boolean removeFirst(Integer element) { return false;}
     public Integer get(int index) { return 0;}
